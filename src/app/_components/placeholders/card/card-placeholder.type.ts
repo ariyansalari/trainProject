a3ea95f4de6eb2d92c430d8871cec26e94eb3b61
ariyansalari <ariyan4509@gmail.com>
2024-03-14 15:50:16 -0700

    feat(page): implement-homePage

---
+++ src/app/_components/placeholders/card/card-placeholder.type.ts
@@ -0,0 +1,4 @@
+export type CardPlaceholderProps={
+    count?:number
+    className?:string
+}
--- src/types/index.ts
+++ src/types/index.ts
@@ -1,3 +1,4 @@
 export * from "./navigationType"
 export * from "./course-summary.interface"
 export * from "./blog-post-summary.interface"
+export * from "./testimonials.type"
