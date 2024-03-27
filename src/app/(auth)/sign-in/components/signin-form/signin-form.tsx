b38a50baabc4364b8f4eb42969c44f9a92ae955b
ariyansalari <ariyan4509@gmail.com>
2024-03-27 11:16:06 -0700

    feat(app): hookForm

--- src/app/(auth)/sign-in/components/signin-form/signin-form.tsx
+++ src/app/(auth)/sign-in/components/signin-form/signin-form.tsx
@@ -1,14 +1,21 @@
 'use client'
 
 import { Button, TextBox } from "@/app"
+import { SignInFormProps } from "@/app/(auth)"
+import { useForm } from "react-hook-form"
 
 export const SignInForm =()=>{
+    const {register,handleSubmit,formState:{errors}}=useForm<SignInFormProps>()
+    const onSubmit=(data:any)=>{
+        console.log(data);
+        
+    }
     return (
         <>
         <h5 className="text-2xl">ورود | ثبت نام</h5>
         <p className="mt-2">دنیای شگفت انگیز برنامه نویسی در انتظار شماست!</p>
-        <form className="flex flex-col gap-6 mt-16">
-          <TextBox placeholder="شماره موبایل"/>
+        <form className="flex flex-col gap-6 mt-16" onSubmit={handleSubmit(onSubmit)}>
+          <TextBox {...register('mobile')} placeholder="شماره موبایل"/>
           <Button type="submit" variant="primary">
             تایید و دریافت کد
           </Button>
