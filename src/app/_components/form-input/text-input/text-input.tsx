1c3f8397759c9d63f886d77ab3957ecb009bc3e0
ariyansalari <ariyan4509@gmail.com>
2024-03-28 05:38:41 -0700

    feat(app): implement-textinput-component

---
+++ src/app/_components/form-input/text-input/text-input.tsx
@@ -0,0 +1,26 @@
+import { FieldValues, get } from "react-hook-form";
+import { TextInputProps } from "./text-input.type";
+import { TextBox } from "@/app";
+
+export const TextInput = <TFormValues extends FieldValues>({
+  name,
+  register,
+  rules,
+  errors,
+  variant,
+  ...rest
+}: TextInputProps<TFormValues>) => {
+  const error = get(errors, name);
+  const hasError = !!error;
+
+  return (
+    <>
+      <TextBox
+        {...register(name, rules)}
+        {...(hasError ? { variant: "error" } : { variant: variant })}
+        {...rest}
+      />
+      {hasError && <p className="mt-2 text-sm text-error">{error.message}</p>}
+    </>
+  );
+};
